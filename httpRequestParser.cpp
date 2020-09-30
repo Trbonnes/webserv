ae0067a01568333c96947f8f0e17f344b9f90f69
Tristan BONNESON <trbonnes@e1r2p14.42.fr>
2020-09-30 17:32:53 +0200

    makefile plus http request inside string

--- httpRequestParser.cpp
+++ httpRequestParser.cpp
@@ -6,7 +6,28 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:45:46 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/09/30 15:45:47 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/09/30 17:32:11 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "socket.hpp"
+
+Socket	*httpRequestParser(int fd) {
+
+	Socket *socket = new Socket(fd);
+	char c[4096];
+	std::string s;
+
+	for (int i = 0; i < 4096; i++)
+		c[i] = '\0';
+	while (int ret = read(fd, c, 4096) > 0) {
+		if (ret == -1) { return NULL; }
+		s.append(c);
+		for (int i = 0; i < 4096; i++)
+			c[i] = '\0';
+	}
+	
+	std::cout << s << std::endl;
+
+	return socket;
+}
--- testMain.cpp
+++ testMain.cpp
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:30:52 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/09/30 15:51:59 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/09/30 17:25:42 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@
 int main() {
 
 	int fd = open("./httpRequestTest", O_RDONLY);
-	Socket socket(fd);
-	
-    std::cout << socket.getFd() << std::endl;
-    std::cout << socket.getMethod() << std::endl;
-    std::cout << socket.getRequestURI() << std::endl;
-    std::cout << socket.gethttpVersion() << std::endl;
-    std::cout << socket.getContentLenght() << std::endl;
-    std::cout << socket.getContentLocation() << std::endl;
-    std::cout << socket.getContentType() << std::endl;
-    std::cout << socket.getDate() << std::endl;
-    std::cout << socket.getTransferEncoding() << std::endl;
-    std::cout << socket.getAcceptCharset() << std::endl;
-    std::cout << socket.getAcceptLanguage() << std::endl;
-    std::cout << socket.getAuthorization() << std::endl;
-    std::cout << socket.getHost() << std::endl;
-    std::cout << socket.getUserAgent() << std::endl;
-	std::cout << socket.getReferer() << std::endl;
-    std::cout << socket.getBody() << std::endl;
+    Socket *socket = httpRequestParser(fd);
+	(void)socket;
+    /*
+    std::cout << socket->getFd() << std::endl;
+    std::cout << socket->getMethod() << std::endl;
+    std::cout << socket->getRequestURI() << std::endl;
+    std::cout << socket->gethttpVersion() << std::endl;
+    std::cout << socket->getContentLength().front() << std::endl;
+    std::cout << socket->getContentLocation().front() << std::endl;
+    std::cout << socket->getContentType().front() << std::endl;
+    std::cout << socket->getDate().front() << std::endl;
+    std::cout << socket->getTransferEncoding().front() << std::endl;
+    std::cout << socket->getAcceptCharset().front() << std::endl;
+    std::cout << socket->getAcceptLanguage().front() << std::endl;
+    std::cout << socket->getAuthorization().front() << std::endl;
+    std::cout << socket->getHost().front() << std::endl;
+    std::cout << socket->getUserAgent().front() << std::endl;
+	std::cout << socket->getReferer().front() << std::endl;
+    std::cout << socket->getBody() << std::endl;
+    */
 }
