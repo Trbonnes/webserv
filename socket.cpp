e0a2e3c5124755e405c23d4529cacc4d5d4f9625
Tristan BONNESON <trbonnes@e1r2p14.42.fr>
2020-09-30 15:13:51 +0200

    first push with socket class copied from methods branch

---
+++ socket.cpp
@@ -0,0 +1,166 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   socket.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 15:10:55 by trbonnes          #+#    #+#             */
+/*   Updated: 2020/09/30 15:11:49 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Socket.hpp"
+
+Socket::Socket() :
+_fd(0),
+_method("GET"),
+_requestURI("/index.html"),
+_httpVersion("HTTP/1.1"),
+_contentLength(""),
+_contentLocation(""),
+_contentType(""),
+_date(""),
+_transferEncoding(""),
+_acceptCharset(""),
+_acceptLanguage("en-US,en;q="".9"),
+_authorization(""),
+_host("localhost"),
+_userAgent("Mozilla/5."" (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/8""."".3987.149 Safari/537.36"),
+_body("") {
+}
+
+Socket::Socket(int fd) :
+_fd(fd),
+_method(""),
+_requestURI(""),
+_httpVersion(""),
+_contentLength(""),
+_contentLocation(""),
+_contentType(""),
+_date(""),
+_transferEncoding(""),
+_acceptCharset(""),
+_acceptLanguage(""),
+_authorization(""),
+_host(""),
+_userAgent(""),
+_body("") {
+    parser();
+}
+
+Socket::Socket(Socket const &copy)
+{
+    _fd = copy._fd;
+    _method = copy._method;
+    _requestURI = copy._requestURI;
+    _httpVersion = copy._httpVersion;
+    _contentLength = copy._contentLength;
+    _contentLocation = copy._contentLocation;
+    _contentType = copy._contentType;
+    _date = copy._date;
+    _transferEncoding = copy._transferEncoding;
+    _acceptCharset = copy._acceptCharset;
+    _acceptLanguage = copy._acceptLanguage;
+    _authorization = copy._authorization;
+    _host = copy._host;
+    _userAgent = copy._userAgent;
+    _body = copy._body;
+}
+
+Socket::~Socket() {}
+
+Socket      &Socket::operator=(Socket const &rhs)
+{
+    _fd = rhs._fd;
+    _method = rhs._method;
+    _requestURI = rhs._requestURI;
+    _httpVersion = rhs._httpVersion;
+    _contentLength = rhs._contentLength;
+    _contentLocation = rhs._contentLocation;
+    _contentType = rhs._contentType;
+    _date = rhs._date;
+    _transferEncoding = rhs._transferEncoding;
+    _acceptCharset = rhs._acceptCharset;
+    _acceptLanguage = rhs._acceptLanguage;
+    _authorization = rhs._authorization;
+    _host = rhs._host;
+    _userAgent = rhs._userAgent;
+    _body = rhs._body;
+    return *this;
+}
+
+void        Socket::parser(void)
+{
+    return ;
+}
+
+std::string     Socket::getMethod()
+{
+    return _method;
+}
+
+std::string     Socket::getRequestURI()
+{
+    return _requestURI;
+}
+
+std::string     Socket::gethttpVersion()
+{
+    return _httpVersion;
+}
+
+std::string     Socket::getContentLenght()
+{
+    return _contentLength;
+}
+
+std::string     Socket::getContentLocation()
+{
+    return _contentLocation;
+}
+
+std::string     Socket::getContentType()
+{
+    return _contentType;
+}
+
+std::string     Socket::getDate()
+{
+    return _date;
+}
+
+std::string     Socket::getTransferEncoding()
+{
+    return _transferEncoding;
+}
+
+std::string     Socket::getAcceptCharset()
+{
+    return _acceptCharset;
+}
+
+std::string     Socket::getAcceptLanguage()
+{
+    return _acceptLanguage;
+}
+
+std::string     Socket::getAuthorization()
+{
+    return _authorization;
+}
+
+std::string     Socket::getHost()
+{
+    return _host;
+}
+
+std::string     Socket::getUserAgent()
+{
+    return _userAgent;
+}
+
+std::string     Socket::getBody()
+{
+    return _body;
+}
---
+++ socket.hpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   socket.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 15:10:02 by trbonnes          #+#    #+#             */
+/*   Updated: 2020/09/30 15:10:34 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOCKET_HPP
+# define SOCKET_HPP
+
+# include <iostream>
+
+class   Socket
+{
+    private:
+
+    // SOCKET
+
+    int             _fd;
+
+    // REQUEST LINE
+
+    std::string     _method;
+    std::string     _requestURI;
+    std::string     _httpVersion;
+
+    // HEADERS
+
+    std::string     _contentLength;
+    std::string     _contentLocation;
+    std::string     _contentType;
+    std::string     _date;
+    std::string     _transferEncoding;
+    std::string     _acceptCharset;
+    std::string     _acceptLanguage;
+    std::string     _authorization;
+    std::string     _host;
+    std::string     _userAgent;
+
+    // BODY
+
+    std::string     _body;
+
+    // PRIVATE FUNCTIONS
+
+    void            parser(void);
+
+    public:
+    Socket();
+    Socket(int fd);
+    Socket(Socket const &copy);
+    ~Socket();
+
+    Socket          &operator=(Socket const &rhs);
+    int             getFd();
+    std::string     getMethod();
+    std::string     getRequestURI();
+    std::string     gethttpVersion();
+    std::string     getContentLenght();
+    std::string     getContentLocation();
+    std::string     getContentType();
+    std::string     getDate();
+    std::string     getTransferEncoding();
+    std::string     getAcceptCharset();
+    std::string     getAcceptLanguage();
+    std::string     getAuthorization();
+    std::string     getHost();
+    std::string     getUserAgent();
+    std::string     getBody();
+};
+
+#endif
