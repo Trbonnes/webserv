9ad6112748ba1312e180a13132f26000e95f062f
Tristan BONNESON <trbonnes@e1r6p8.42.fr>
2020-10-01 15:47:13 +0200

    all non-Accept Headers parsed

--- parseHeaders.cpp
+++ parseHeaders.cpp
@@ -6,55 +6,14 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 18:55:18 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/10/01 15:18:41 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/10/01 15:43:09 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.hpp"
 
-void ParseAcceptCharsets(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
 
-void ParseAcceptLanguage(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseAutorization(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseContentLength(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseContentLocation(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseContentType(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseDate(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
-}
-
-void ParseHost(Socket *socket, std::string request, size_t pos) {
+std::string ParseStdHeaders(std::string request, size_t pos) {
 	std::string s;
 
 	while (request[pos] && request[pos] != ':') { pos++; }
@@ -63,23 +22,56 @@ void ParseHost(Socket *socket, std::string request, size_t pos) {
 	while (request[pos] && request[pos] != '\n') {
 		s.push_back(request[pos++]);
 	}
-	socket->setHost(s);
+	return s;
+}
+
+std::vector<std::string> ParseAcceptHeaders(std::string request, size_t pos) {
+	std::vector<std::string> v;
+	(void)request;
+	(void)pos;
+	return v;
+}
+
+void ParseAcceptCharsets(Socket *socket, std::string request, size_t pos) {
+	socket->setAcceptCharset(ParseAcceptHeaders(request, pos));
+}
+
+void ParseAcceptLanguage(Socket *socket, std::string request, size_t pos) {
+	socket->setAcceptLanguage(ParseAcceptHeaders(request, pos));
+}
+
+void ParseAutorization(Socket *socket, std::string request, size_t pos) {
+	socket->setAuthorization(ParseStdHeaders(request, pos));
+}
+
+void ParseContentLength(Socket *socket, std::string request, size_t pos) {
+	socket->setContentLength(ParseStdHeaders(request, pos));
+}
+
+void ParseContentLocation(Socket *socket, std::string request, size_t pos) {
+	socket->setContentLocation(ParseStdHeaders(request, pos));
+}
+
+void ParseContentType(Socket *socket, std::string request, size_t pos) {
+	socket->setContentType(ParseStdHeaders(request, pos));
+}
+
+void ParseDate(Socket *socket, std::string request, size_t pos) {
+	socket->setDate(ParseStdHeaders(request, pos));
+}
+
+void ParseHost(Socket *socket, std::string request, size_t pos) {
+	socket->setHost(ParseStdHeaders(request, pos));
 }
 
 void ParseReferer(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
+	socket->setReferer(ParseStdHeaders(request, pos));
 }
 
 void ParseTransferEncoding(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
+	socket->setTransferEncoding(ParseStdHeaders(request, pos));
 }
 
 void ParseUserAgent(Socket *socket, std::string request, size_t pos) {
-	(void)socket;
-	(void)request;
-	(void)pos;
+	socket->setUserAgent(ParseStdHeaders(request, pos));
 }
--- testMain.cpp
+++ testMain.cpp
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:30:52 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/10/01 15:19:18 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/10/01 15:46:15 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,21 @@ int main() {
 	int fd = open("./httpRequestTest", O_RDONLY);
     Socket *socket = httpRequestParser(fd);
 
-    std::cout << socket->getFd() << std::endl;
-    std::cout << socket->getMethod() << std::endl;
-	std::cout << socket->getRequestURI() << std::endl;
-    std::cout << socket->getHttpVersion() << std::endl;
-    /*
-    std::cout << socket->getContentLength().front() << std::endl;
-    std::cout << socket->getContentLocation().front() << std::endl;
-    std::cout << socket->getContentType().front() << std::endl;
-    std::cout << socket->getDate().front() << std::endl;
-    std::cout << socket->getTransferEncoding().front() << std::endl;
-    std::cout << socket->getAcceptCharset().front() << std::endl;
-    std::cout << socket->getAcceptLanguage().front() << std::endl;
-    std::cout << socket->getAuthorization().front() << std::endl;
-    */
-    std::cout << socket->getHost() << std::endl;
-    /*
-    std::cout << socket->getUserAgent().front() << std::endl;
-	std::cout << socket->getReferer().front() << std::endl;
-    std::cout << socket->getBody() << std::endl;
-    */
+    std::cout << "Fd: " << socket->getFd() << std::endl;
+    std::cout << "Method: " << socket->getMethod() << std::endl;
+	std::cout << "URI: " << socket->getRequestURI() << std::endl;
+    std::cout << "Version: " << socket->getHttpVersion() << std::endl;
+
+    std::cout << "ContentType: " << socket->getContentType() << std::endl;
+    std::cout << "getDate: " << socket->getDate() << std::endl;
+    std::cout << "TransferEncoding: " << socket->getTransferEncoding() << std::endl;
+    //std::cout << "AcceptCharset: " << socket->getAcceptCharset() << std::endl;
+    //std::cout << "AcceptLanguage: " << socket->getAcceptLanguage() << std::endl;
+    std::cout << "Authorization: " << socket->getAuthorization() << std::endl;
+    std::cout << "Host: " << socket->getHost() << std::endl;
+    std::cout << "UserAgent: " << socket->getUserAgent() << std::endl;
+	std::cout << "Referer: " << socket->getReferer() << std::endl;
+    std::cout << "ContentLocation: " << socket->getContentLocation() << std::endl;
+    std::cout << "ContentLength: " << socket->getContentLength() << std::endl;
+    //std::cout << "Body: " << socket->getBody() << std::endl;
 }
