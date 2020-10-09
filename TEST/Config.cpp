67071643eed01df3d1406039be6475fa075115e0
Pauline GANGLOFF <pganglof@e2r9p4.42.fr>
2020-10-09 16:49:47 +0200

    add alias OK

--- TEST/Config.cpp
+++ TEST/Config.cpp
@@ -1,23 +1,23 @@
 #include "Config.hpp"
 
 Config::Config() :
-_defaultRoot("/home/pauline/webserver/www"),
+_defaultRoot("/sgoinfre/goinfre/Perso/pganglof/webserver/www"),
 _defaultServerName("localhost"),
 _defaultIndex(0),
 _defaultType("text/plain"),
 _defaultCharset("koi8-r"),
 _defaultLanguage(0),
-_configFilesRoot("/home/pauline/webserver/config"),
+_configFilesRoot("/sgoinfre/goinfre/Perso/pganglof/webserver/config"),
 _defaultAuth_basic("\"Authorization\""),
-_defaultAuth_basic_user_file("/home/pauline/webserver/config/.htpasswd"),
+_defaultAuth_basic_user_file("/sgoinfre/goinfre/Perso/pganglof/webserver/config/.htpasswd"),
 _defaultAutoindex("off")
 {
     _defaultAllow.push_back("GET");
     _defaultAllow.push_back("HEAD");
     _defaultAllow.push_back("POST");
     _defaultAllow.push_back("PUT");
+    _defaultLanguage.push_back("en");
     _defaultLanguage.push_back("fr");
-    // _defaultLanguage.push_back("en");
     _defaultIndex.push_back("index.html");
     // _defaultIndex.push_back("index.php");
 
@@ -62,9 +62,9 @@ _defaultAutoindex("off")
     
     index.push_back("index.php");
 
-    Location loc1("/data/", "/home/pauline/webserver/www",
+    Location loc1("/data/", "/sgoinfre/goinfre/Perso/pganglof/webserver/www",
     _defaultAllow, index,
-    "text/html", "utf-8", _defaultLanguage, _defaultAuth_basic, _defaultAuth_basic_user_file, _defaultAutoindex);
+    "text/html", "utf-8", _defaultLanguage, _defaultAuth_basic, _defaultAuth_basic_user_file, _defaultAutoindex, "/blabla/");
 
     _locationList["/data/"] = loc1;
 
@@ -74,15 +74,15 @@ _defaultAutoindex("off")
 
     index2.push_back("42.png");
 
-    Location loc2("/images/", "/home/pauline/webserver/www",
+    Location loc2("/images/", "/sgoinfre/goinfre/Perso/pganglof/webserver/www",
     _defaultAllow, index2,
-    "text/html", "", _defaultLanguage, "off", "", _defaultAutoindex);
+    "text/html", "", _defaultLanguage, "off", "", _defaultAutoindex, "");
 
     _locationList["/images/"] = loc2;
 
-    Location loc3("/", "/home/pauline/webserver/www",
+    Location loc3("/", "/sgoinfre/goinfre/Perso/pganglof/webserver/www",
     _defaultAllow, _defaultIndex,
-    _defaultType, _defaultCharset, _defaultLanguage, "off", "", "on");
+    _defaultType, _defaultCharset, _defaultLanguage, "off", "", "on", "");
 
     _locationList["/"] = loc3;
 
@@ -148,7 +148,7 @@ std::string             Config::getRoot(std::string location)
     return _defaultRoot;
 }
 
-std::string             Config::getServerName(std::string location)
+std::string             Config::getServerName()
 {
     return _defaultServerName;
 }
@@ -284,4 +284,20 @@ std::string                 Config::getAutoindex(std::string location)
         itBegin++;
     }
     return _defaultAutoindex;   
+}
+
+std::string             Config::getAlias(std::string location)
+{
+    std::map<std::string, Location, Compare<std::string> >::iterator itBegin;
+    std::map<std::string, Location, Compare<std::string> >::iterator itEnd;  
+
+    itBegin = _locationList.begin();
+    itEnd = _locationList.end();
+    while (itBegin != itEnd)
+    {
+        if (location.compare(itBegin->first) == 0)
+            return (itBegin->second._alias);
+        itBegin++;
+    }
+    return "";   
 }
