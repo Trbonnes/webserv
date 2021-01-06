b7c874fbf7ddcffaeff28ffee91fd89f4a24ff1f
Tristan BONNESON <trbonnes@e1r13p12.42.fr>
2021-01-06 09:20:28 +0100

    splitting first part of host

--- HTTP/parseHeaders.cpp
+++ HTTP/parseHeaders.cpp
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 18:55:18 by trbonnes          #+#    #+#             */
-/*   Updated: 2021/01/06 09:16:51 by trbonnes         ###   ########.fr       */
+/*   Updated: 2021/01/06 09:20:19 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -135,7 +135,7 @@ void ParseDate(Socket *socket, std::string request, size_t pos) {
 void ParseHost(Socket *socket, std::string request, size_t pos) {
 	std::string host = ParseStdHeaders(request, pos);
 	pos = host.find(":");
-	socket->setHost(host.substr(0, pos - 1);
+	socket->setHost(host.substr(0, pos - 1));
 	pos++;
 	socket->setPort(stoi(host.substr(pos, host.npos)));
 }
