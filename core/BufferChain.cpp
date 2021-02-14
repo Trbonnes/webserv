be17c8620d6df841df457cba975d9aa737f282ab
yorn <yornclaudel@gmail.com>
2021-02-14 19:08:06 +0100

    added buferchain class

---
+++ core/BufferChain.cpp
@@ -0,0 +1,83 @@
+#include "BufferChain.hpp"
+
+char g_read_medium[BUFFER_SIZE_MEDIUM] = {};
+char g_read_large[BUFFER_SIZE_LARGE] = {};
+
+BufferChain::BufferChain()
+{
+}
+
+BufferChain::BufferChain(BufferChain&)
+{
+	// TO DO add deep copy
+}
+
+BufferChain& BufferChain::operator=(BufferChain&)
+{
+	// TO DO add flush then deep copy
+}
+
+BufferChain::~BufferChain()
+{
+}
+
+void	BufferChain::pushBack(char* toadd, size_t size)
+{
+	buffer_t pair;
+	pair.data = toadd;
+	pair.size = size;
+	_chain.push(pair);
+}
+
+void	BufferChain::copyPushBack(char *tocopy, size_t size)
+{
+	char* n = new char[size]();
+
+	std::memcpy(n, tocopy, std::min(size, BUFFER_SIZE_LARGE));
+	pushBack(n, size);
+}
+
+buffer_t& BufferChain::getFirst()
+{
+	return _chain.front()
+}
+
+buffer_t BufferChain::popFirst()
+{
+	buffer_t pair = _chain.front();
+	_chain.pop();
+	return pair;
+}
+
+void	BufferChain::flush();
+{
+	// TO DO deep destruction
+}
+
+int		BufferChain::writeBufferToFd(BufferChain& chain, FD fd)
+{
+	int ret;
+	BufferChain::buffer_t buff;
+	
+	buff = chain.popFirst();
+	ret = write(fd, buff.data, buff.size);
+	if (ret == -1)
+		throw IOError();
+	return ret;
+}
+
+int		BufferChain::readIntoBuffer(BufferChain& chain, FD fd)
+{
+	int ret;
+	
+	ret = read(fd, g_read_large, size);
+	if (ret == -1)
+		throw IOError();
+	chain.copyPushBack(g_read_large, ret);
+	return ret;
+}
+
+virtual const char* IOError::what() const throw()
+{
+	return "An input or output eror has occurred";
+}
---
+++ core/BufferChain.hpp
@@ -0,0 +1,69 @@
+#if !defined(BUFFER_CHAIN)
+#define BUFFER_CHAIN
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <queue>
+#include <cstring> 
+
+// Socket/Stream read write buffer
+// default nginx client buffer size on 64 bit systems 
+// It should be enough to hold most Http request headers, but we got to implement the case where it is too small
+#define BUFFER_SIZE_MEDIUM 16384
+#define BUFFER_SIZE_LARGE 1024000
+
+
+// Buffer used in socket and filesystem read
+extern char g_read_medium[BUFFER_SIZE_MEDIUM];
+extern char g_read_large[BUFFER_SIZE_LARGE];
+
+// This typedef makes fd manipulation a little bit more clear (thanks to @cchudant for this)
+typedef int FD;
+// This class holds a list of char buffers
+// They can vary in size
+class BufferChain
+{
+
+private:
+	// The buffer pair structure
+	typedef struct	buffer_s
+	{
+		char *data;
+		size_t size;
+	}				buffer_t;
+
+	// The actual buffer list
+	std::queue<buffer_t> _chain;
+
+public:
+
+	// Cooplien
+	BufferChain();
+	BufferChain(BufferChain&);
+	BufferChain &operator=(BufferChain&);
+	~BufferChain();
+
+	// Adds a buffer to the list
+	void	pushBack(char *, size_t);
+	// Copy into a new buffer that is added to the list
+	void	copyPushBack(char *, size_t);
+
+	// return first buffer pair;
+	buffer_t& getFirst();
+	
+	// pop and returns first buffer pair;
+	buffer_t popFirst();
+
+	// Destroy and free every buffer in the list
+	void	flush();
+	static int	writeBufferToFd(BufferChain&, FD);
+	static int	readBuffer(BufferChain&, FD, size_t);
+};
+
+class	IOError: public std::exception {
+	public:
+		virtual const char* what() const throw();
+};
+
+// Non-class members to write and read directly to/from a BufferChain
+#endif // BUFFER_CHAIN
