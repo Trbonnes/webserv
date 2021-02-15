99f7eed260c04e0ee28866833ae2a7f48c9a1705
yorn <yornclaudel@gmail.com>
2021-02-15 14:55:53 +0100

    adding new http module

---
+++ HTTP/Http.hpp
@@ -0,0 +1,57 @@
+#if !defined(HTTP)
+#define HTTP
+
+
+#include "Connection.hpp"
+#include "BufferChain.hpp"
+// This is the entrypoint of the http module
+// All of the filesystem write/read are handled by this module
+// The connection socket read and write are handled by the Worker itself
+
+class Http
+{
+
+typedef int FD;
+
+private:
+	// stream in (CGI or filestream)
+	FD _in;
+
+	// stream out
+	FD _out;
+
+	// reference to the upstream connection
+	Connection& _c;
+
+	// Headers of the request
+	httpRequest _req;
+
+	// Header of the response
+	httpResponse _rep;
+
+	// Pointers to socket buffer's chain
+	BufferChain		_stream_read_chain;
+	BufferChain		_stream_write_chain;
+
+	// Reference to connection socket buffer chains
+	BufferChain&		_read_chain;
+	BufferChain&		_write_chain;
+
+public:
+	Http();
+	~Http();
+	~Http();
+
+};
+
+Http::Http(/* args */)
+{
+}
+
+Http::~Http()
+{
+}
+
+
+
+#endif // HTTP
