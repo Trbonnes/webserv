971217b502d96803b15f87981b803d0cca84efd1
yorn <yornclaudel@gmail.com>
2021-03-25 02:21:49 +0100

    added custom error pages

--- http/response/Error.cpp
+++ http/response/Error.cpp
@@ -4,32 +4,41 @@ Error::Error(ConfigServer* config, HttpRequest* req, std::string& route, std::st
 {
 	_statusCode = statusCode;
 
-	// int         fd;
+	int        		fd;
+	std::string*	buff = NULL;
 
-    // _route = _config->getHTMLErrorPage(_statusCode);
-    // fd = open(_route.c_str(), O_RDONLY);
-    // if (fd == -1)
-    // {
-	std::string* buff = new std::string();
-	buff->append("<!DOCTYPE html>\n<html>\n<body>\n\n<h1>");
-	char *tmp = ft_itoa(_statusCode);
-	buff->append(tmp).append(" ").append(_mapCodes.codes[_statusCode]);
-	free(tmp);
-	buff->append("</h1>\n\n</body>\n</html>\n");
-	_contentType = "text/html";
-	_charset = "utf-8";
-	_contentLength = buff->length();
+	_route = _config->getHTMLErrorPage(_statusCode);
+	fd = open(_route.c_str(), O_RDONLY);
+	if (fd != -1)
+	{
+		struct stat file;
+		stat(_route.c_str(), &file);
+		setContentType();
+		setCharset();
+		_contentLength = file.st_size;
+		setServerName();
+		setContentLocation();
+		_streamReadFd = fd;
+		_state.read = DONE; // TO DO is it ok ? same in File Download
+		_state.readStream = READY;
+	}
+	else
+	{
+		buff = new std::string();
+		buff->append("<!DOCTYPE html>\n<html>\n<body>\n\n<h1>");
+		char *tmp = ft_itoa(_statusCode);
+		buff->append(tmp).append(" ").append(_mapCodes.codes[_statusCode]);
+		free(tmp);
+		buff->append("</h1>\n\n</body>\n</html>\n");
+		_contentType = "text/html";
+		_charset = "utf-8";
+		_contentLength = buff->length();
+	}
 
 	//appending the buffers
 	writeChain.pushBack(getRawHeaders());
-	writeChain.pushBack(buff);
-	// std::cout << buff<< std::endl;
-	// }
-    // else
-    // {
-    //     close(fd);
-    //     get(true);
-    // }
+	if (buff)
+		writeChain.pushBack(buff);
 	std::cout << "/* message */ " << _mapCodes.codes[_statusCode] << std::endl;
 }
 
